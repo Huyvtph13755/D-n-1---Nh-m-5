04b444399c898a101349fd1c1a3f44c8bcde4d96
Mydtph <79831056+MyPH13712@users.noreply.github.com>
2021-11-15 18:57:12 +0700

    thêm product.php

--- duan1/model/product.php
+++ duan1/model/product.php
@@ -1,8 +1,31 @@
 <?php
-// function load4product(){
-//     $sql = "SELECT * FROM product ORDER BY view DESC LIMIT 0,9";
-//     $s = exeQuery($sql, true); //Truy vấn nhiều sp
-// }
+ 
+function load4product(){
+    $sql = "SELECT * FROM product ORDER BY view DESC LIMIT 0,9";
+    $s = exeQuery($sql, true); //Truy vấn nhiều sp
+    return $s;
+}
+function load_name_subcategory($sub_id)
+{
+    if ($sub_id > 0) {
+        $sql = "select * from danhmuc where id=" . $sub_id;
+        $sub = exeQuery($sql, false);
+        extract($sub);
+        return $name;
+    } else {
+        return "";
+    }
+}
+function loadall_sanpham($sub_id = 0)
+{
+    $sql = "select * from sanpham where 1";
+    if ($sub_id > 0) {
+        $sql .= " and sub_id ='" . $sub_id . "' ";
+    }
+    $sql .= " order by id desc";
+    $listpro = exeQuery($sql,true);
+    return $listpro;
+}
 
 // Code của Huy
 // lấy 1 sản phẩm theo id
