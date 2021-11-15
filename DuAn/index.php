2db458acf90636ffea5fc164fb249d32fa8df511
Huyvtph13755 <93895164+Huyvtph13755@users.noreply.github.com>
2021-11-15 12:33:35 +0700

    ccx

--- DuAn/index.php
+++ DuAn/index.php
@@ -13,12 +13,12 @@ if ((isset($_GET['act'])) && ($_GET['act'] != "")) {
     $act = $_GET['act'];
     switch ($act) {
         case 'product_detail':
-            // $load_one = product_detail(1);
-            // $getMinPr = getMinPrice(1);
-            // $f_cl = full_color(1);
-            // $wa = full_warranty();
-            // $gCmt = getAllCmt(1);
-            // $ranPr = getRanPr();
+            $load_one = product_detail(1);
+            $getMinPr = getMinPrice(1);
+            $f_cl = full_color(1);
+            $wa = full_warranty();
+            $gCmt = getAllCmt(1);
+            $ranPr = getRanPr();
             include 'View/product_detail.php';
             break;
         case 'cart':
