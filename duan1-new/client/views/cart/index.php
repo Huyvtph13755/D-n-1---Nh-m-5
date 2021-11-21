b114d9ef510f838e9c347da58c1ea50718983e92
Hoangvm02 <hoangvmph13792@fpt.edu.vn>
2021-11-21 16:45:49 +0700

    them payment

--- duan1-new/client/views/cart/index.php
+++ duan1-new/client/views/cart/index.php
@@ -1,14 +1,187 @@
-<?php
-function cart(){
-    
-}
+   <!-- BANNER STRAT -->
+   <div class="banner inner-banner">
+       <div class="container">
+           <div class="bread-crumb mtb-60 center-xs">
+               <div class="page-title">Women</div>
+               <div class="bread-crumb-inner right-side float-none-xs">
+                   <ul>
+                       <li><a href="index.html">Home</a><i class="fa fa-angle-right"></i></li>
+                       <li><span>Women</span></li>
+                   </ul>
+               </div>
+           </div>
+       </div>
+   </div>
+   <!-- BANNER END -->
 
+   <!-- CONTAIN START -->
+   <section class="container">
+       <div class="pb-85 pt-55">
+           <div class="row">
+               <div class="col-xs-12 mb-xs-30">
+                   <div class="cart-item-table commun-table">
+                       <div class="table-responsive">
+                           <table class="table">
+                               <thead>
+                                   <tr>
+                                       <th>Ảnh</th>
+                                       <th>Tên sản phẩm</th>
+                                       <th>Giá</th>
+                                       <th>Gói bảo hành</th>
+                                       <th>Số lượng</th>
+                                       <th>Tổng giá</th>
+                                       <th>Xóa</th>
+                                   </tr>
+                               </thead>
+                               <tbody>
+                                   <tr>
+                                       <td>
+                                           <a href="product-page.html">
+                                               <div class="product-image"><img alt="Streetwear" src="images/1.jpg"></div>
+                                           </a>
+                                       </td>
+                                       <td>
+                                           <div class="product-title">
+                                               <a href="product-page.html">Cross Colours Camo Print Tank half mengo</a>
+                                           </div>
+                                       </td>
+                                       <td>
+                                           <ul>
+                                               <li>
+                                                   <div class="base-price price-box">
+                                                       <span class="price">$80.00</span>
+                                                   </div>
+                                               </li>
+                                           </ul>
+                                       </td>
+                                       <td>
+                                           <ul>
+                                               <li>
+                                                   <div class="base-price price-box">
+                                                       <span class="price">$80.00</span>
+                                                   </div>
+                                               </li>
+                                           </ul>
+                                       </td>
+                                       <td>
+                                           <div class="input-box">
+                                               <select data-id="100" class="quantity_cart" name="quantity_cart">
+                                                   <option selected="" value="1">1</option>
+                                                   <option value="2">2</option>
+                                                   <option value="3">3</option>
+                                                   <option value="4">4</option>
+                                               </select>
+                                           </div>
+                                       </td>
+                                       <td>
+                                           <div class="total-price price-box">
+                                               <span class="price">$80.00</span>
+                                           </div>
+                                       </td>
+                                       <td>
+                                           <i title="Remove Item From Cart" data-id="100" class="fa fa-trash cart-remove-item"></i>
+                                       </td>
+                                   </tr>
+                                   <tr>
+                                       <td>
+                                           <a href="product-page.html">
+                                               <div class="product-image"><img alt="Streetwear" src="images/2.jpg"></div>
+                                           </a>
+                                       </td>
+                                       <td>
+                                           <div class="product-title">
+                                               <a href="product-page.html">Defyant Reversible Dot Shorts</a>
+                                           </div>
+                                       </td>
+                                       <td>
+                                           <ul>
+                                               <li>
+                                                   <div class="base-price price-box">
+                                                       <span class="price">$80.00</span>
+                                                   </div>
+                                               </li>
+                                           </ul>
+                                       </td>
+                                       <td>
+                                           <div class="input-box">
+                                               <select data-id="100" class="quantity_cart" name="quantity_cart">
+                                                   <option selected="" value="1">1</option>
+                                                   <option value="2">2</option>
+                                                   <option value="3">3</option>
+                                                   <option value="4">4</option>
+                                               </select>
+                                           </div>
+                                       </td>
+                                       <td>
+                                           <div class="total-price price-box">
+                                               <span class="price">$80.00</span>
+                                           </div>
+                                       </td>
+                                       <td>
+                                           <i title="Remove Item From Cart" data-id="100" class="fa fa-trash cart-remove-item"></i>
+                                       </td>
+                                   </tr>
+                               </tbody>
+                           </table>
+                       </div>
+                   </div>
+               </div>
+           </div>
+           <div class="mb-30">
+               <div class="row">
+                   <div class="col-sm-6">
+                       <div class="mt-30">
+                           <a href="shop.html" class="btn btn-black"><span><i class="fa fa-angle-left"></i></span>Continue Shopping</a>
+                       </div>
+                   </div>
+                   <div class="col-sm-6">
+                       <div class="mt-30 right-side float-none-xs">
+                           <a class="btn btn-black">Trạng thái</a>
+                       </div>
+                   </div>
+               </div>
+           </div>
+           <hr>
+           <div class="mtb-30">
+               <div class="row">
+                   <div class="col-sm-6 mb-xs-40">
+                   </div>
+                   <div class="col-sm-6">
+                       <div class="cart-total-table commun-table">
+                           <div class="table-responsive">
+                               <table class="table">
+                                   <thead>
+                                       <tr>
+                                           <th colspan="2">Cart Total</th>
+                                       </tr>
+                                   </thead>
+                                   <tbody>
 
-
-
-
-
-
-
-
-?>
+                                       <tr>
+                                           <td><b>Amount Payable</b></td>
+                                           <td>
+                                               <div class="price-box">
+                                                   <span class="price"><b>$160.00</b></span>
+                                               </div>
+                                           </td>
+                                       </tr>
+                                   </tbody>
+                               </table>
+                           </div>
+                       </div>
+                   </div>
+               </div>
+           </div>
+           <hr>
+           <div class="mt-30">
+               <div class="row">
+                   <div class="col-xs-12">
+                       <div class="right-side float-none-xs">
+                           <a href="checkout.html" class="btn btn-black">Proceed to checkout<span><i class="fa fa-angle-right"></i></span></a>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </div>
+   </section>
+   <!-- CONTAINER END -->
