91c26d019b3088f83ef603071aae5ee1f15da90e
Mydtph <79831056+MyPH13712@users.noreply.github.com>
2021-11-21 13:23:34 +0700

    Update utils.php

---
+++ duan1-new/commons/utils.php
@@ -0,0 +1,24 @@
+<?php
+
+const BASE_URL = "http://localhost/DuAn1-Nhom5/duan1-new/";
+const ADMIN_URL = BASE_URL . 'cp-admin/';
+const ADMIN_ASSET = BASE_URL . 'public/admin-assets/';
+const CLIENT = BASE_URL . 'public/';
+// const CLIENT_URL = BASE_URL . '';
+
+
+
+
+function client_render($view, $data = []){
+    extract($data);
+    $view = './client/views/' . $view;
+    include_once "./client/views/layouts/main.php";
+}
+
+// function admin_render($view, $data = [], $jsFile = null){
+//     extract($data);
+//     $view = './admin/views/' . $view;
+//     include_once "./admin/views/layouts/main.php";
+// }
+
+?>
