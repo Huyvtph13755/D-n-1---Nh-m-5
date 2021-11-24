012318d51e26e10139774b0ab8f2587681b2d151
Huyvtph13755 <93895164+Huyvtph13755@users.noreply.github.com>
2021-11-25 00:48:41 +0700

    done-giao-dien-sp

---
+++ duan1-new/admin/views/product/creat-new-product.php
@@ -0,0 +1,49 @@
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Tạo mới sản phẩm</h3>
+            </div>
+            <div class="card-body">
+                <form action="<?= ADMIN_URL . 'danh-muc/luu-tao-moi' ?>" method="post">
+                    <div class="col-6 offset-3">
+                        <div class="form-group">
+                            <label for="">Tên danh mục: </label>
+                            <select name="subcategory_id" id="" style="outline:none; width: 200px; border: 1px solid #AAAAAA; border-radius: 5px; margin-left: 10px; padding-left: 10px;">
+                                <?php foreach ($subcate as $index => $l) : ?>
+                                    <option value="<?= $l['subcategory_id'] ?>"><?= $l['name_subcategory'] ?></option>
+                                <?php endforeach ?>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                          <label for="">Tên sản phẩm: </label>
+                          <input type="text" name="name_product" class="form-control" placeholder="" aria-describedby="helpId">
+                        </div>
+                        <div class="form-group">
+                          <label for="">Ảnh: </label><br>
+                          <input type="file" name="image_product">
+                        </div>
+                        <div class="form-group">
+                          <label for="">Thời gian bảo hành (tháng): </label>
+                          <input type="number" name="warranty" class="form-control" placeholder="" aria-describedby="helpId">
+                        </div>
+                        <div class="form-group">
+                          <label for="">Mô tả ngắn sản phẩm: </label>
+                          <textarea class="form-control" name="sub_decription" id="box_text" cols="50" rows="3"></textarea>
+                        </div>
+                        <div class="form-group">
+                          <label for="">Mô tả chi tiết sản phẩm: </label>
+                          <textarea class="form-control" name="decription" id="box_text" cols="50" rows="6"></textarea>
+                        </div>
+                        <br>
+                        <div class="d-flex justify-content-center">
+                            <a href="<?= ADMIN_URL . 'product' ?> " class="btn btn-sm btn-danger" style="width: 50%; font-weight: bold;" >Hủy</a>
+                            &nbsp;
+                            <button type="submit" style="width: 50%; font-weight: bold;" class="btn btn-sm btn-primary">Tạo mới</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ duan1-new/admin/views/product/update-color.php
@@ -0,0 +1,33 @@
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Thêm màu sắc</h3>
+            </div>
+            <div class="card-body">
+                <form action="<?= ADMIN_URL . 'danh-muc/luu-tao-moi' ?>" method="post">
+                    <div class="col-6 offset-3">
+                        <div class="form-group">
+                            <label for="">Tên màu sắc: </label>
+                            <input type="text" value="<?= $upc['name_color']?>" name="name_color" class="form-control" placeholder="" aria-describedby="helpId">
+                        </div>
+                        <div class="form-group">
+                          <label for="">Giá thêm (đồng): </label>
+                          <input type="number" value="<?= $upc['price_add']?>" name="price_add" class="form-control" placeholder="" aria-describedby="helpId">
+                        </div>
+                        <div class="form-group">
+                          <label for="">Số lượng: </label>
+                          <input type="number" value="<?= $upc['quantity']?>" name="quantity" class="form-control" placeholder="" aria-describedby="helpId">
+                        </div>
+                        <br>
+                        <div class="d-flex justify-content-center">
+                            <a href="<?= ADMIN_URL . 'product/detail-product?product_id='.$_GET['product_id'] ?> " class="btn btn-sm btn-danger" style="width: 50%; font-weight: bold;" >Hủy</a>
+                            &nbsp;
+                            <button type="submit" style="width: 50%; font-weight: bold;" class="btn btn-sm btn-primary">Lưu</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
